b3f1277ec576f56cad953eaa59a498789f9f4783
punchthatface <andrewkim0621@gmail.com>
2026-04-21 12:26:50 -0400

    chmod to script and better smoke test

--- apps/spi_test/spi_test.c
+++ apps/spi_test/spi_test.c
@@ -4,30 +4,26 @@
 
 int main(void)
 {
-    printf("start\n");
-
-    // Put Bank 0 into SPI mode
     eff_pinmux_set(PINMUX_0, PINMUX_SPI);
-    printf("pinmux ok\n");
 
     eff_spi_cfg_t cfg = EFF_SPI_DEFAULTS;
     cfg.xfer_mode = SPI_XFER_WRITE_ONLY;
     cfg.bus_size  = SPI_BUS_SINGLE;
 
-    int8_t rc = eff_spi_init(SPI_0, &cfg);
-    if (rc != 0) {
-        printf("spi_init failed: %d\n", rc);
+    if (eff_spi_init(SPI_0, &cfg) != 0) {
+        printf("spi_init failed\n");
         return -1;
     }
-    printf("spi_init ok\n");
 
-    uint8_t tx[4] = {0xAA, 0x55, 0x12, 0x34};
+    uint8_t tx[16] = {
+        0xAA, 0x55, 0xAA, 0x55,
+        0xF0, 0x0F, 0xCC, 0x33,
+        0xAA, 0x55, 0xAA, 0x55,
+        0xF0, 0x0F, 0xCC, 0x33
+    };
 
     while (1) {
-        printf("xfer\n");
         eff_spi_xfer(SPI_0, 0, 0, tx, sizeof(tx), 0, 0);
         sleep(1);
     }
-
-    return 0;
 }
